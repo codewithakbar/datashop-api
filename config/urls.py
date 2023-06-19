2837a5f804454002495b6264e73260d429ffe10f
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-06-19 18:45:28 +0500

    url steeitbng

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 from rest_framework import routers
 
@@ -34,4 +36,8 @@ urlpatterns = [
     path("user/", include("users.urls", namespace="users")),
     path('', include(router.urls)),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
