9eb924d78dd9f1b6f043ae4a9b3b04a4e232c153
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-06-30 17:13:04 +0500

    zaybal media :de:

--- config/urls.py
+++ config/urls.py
@@ -40,4 +40,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+# urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
